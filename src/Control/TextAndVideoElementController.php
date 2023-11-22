4228db1d9b412fc2ec15369fa5d9177f11e40b8f
Tim Burt <tim@dorset-digital.net>
2023-11-22 14:29:57 +0000

    Element configs

---
+++ src/Control/TextAndVideoElementController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BiffBangPow\Theme\BaseTheme\Control;
+
+use DNADesign\Elemental\Controllers\ElementController;
+use SilverStripe\View\Requirements;
+use SilverStripe\View\ThemeResourceLoader;
+
+class TextAndVideoElementController extends ElementController
+{
+    public function init()
+    {
+        parent::init();
+        $themeCSS = ThemeResourceLoader::inst()->findThemedCSS('client/dist/css/elements/textandvideo');
+        if ($themeCSS) {
+            Requirements::css($themeCSS, '', ['defer' => true]);
+        }
+
+        if ($this->element->VideoType === 'youtube') {
+            $themeJS = ThemeResourceLoader::inst()->findThemedResource('node_modules/@justinribeiro/lite-youtube/lite-youtube.js');
+            Requirements::javascript($themeJS, [
+                'type' => 'module',
+                'async' => true,
+                'defer' => true
+            ]);
+        }
+    }
+}
